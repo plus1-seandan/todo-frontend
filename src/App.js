000d0c6fae838420a3e520f27e7d6e931f5b34eb
Sean Dan <seandan@unknowna483e75b7349.attlocal.net>
2022-07-17 17:56:00 -0500

    get tasks from my backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchMyTasksFromMyBackend() {
+      // You can await here
+      const tasks = await axios.get(`http://localhost:5000/tasks`);
+      setTasks(tasks.data);
+    }
+    fetchMyTasksFromMyBackend();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Tasks</h1>
+      {tasks.length ? (
+        tasks.map((task, index) => (
+          <div>  
+            <p>{index+1}. {task.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No Tasks Yet! Create One!</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
